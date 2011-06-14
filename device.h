aae212b446a3a65ede119eefdadb7c91d41cb167
rollercoaster <rollercoaster@fc751244-6d69-0410-b144-98fd9a06952e>
2011-06-14 08:28:55 +0000

    pi mcli: -  re-enable patch r179 agains "channel not available"-message from vdr

--- device.h
+++ device.h
@@ -71,6 +71,7 @@ class cMcliDevice:public cDevice
 	cMcliFilters *m_filters;
 	cMcliDevice (void);
 	virtual ~ cMcliDevice ();
+	virtual bool Ready();
 	void SetMcliRef(cPluginMcli *m)
 	{
 		m_mcli=m;
